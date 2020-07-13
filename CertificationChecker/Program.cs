e4a983a359bca6ee0a6de397efc56eab85db169b
yueyinqiu <lujiaqi2003@126.com>
2020-07-13 16:21:34 +0800

    updt

---
+++ CertificationChecker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CertificationChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSA rsa = RSA.Create();
+
+            Console.Write("Enter the path of the public key: ");
+            var key = Console.ReadLine();
+            rsa.ImportRSAPublicKey(File.ReadAllBytes(key), out _);
+
+            Console.Write("Enter the path of the file: ");
+            var fileName = Console.ReadLine();
+
+            Console.Write("Enter the path of the signature: ");
+            var signName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            using FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            var hash = MD5.Create().ComputeHash(fileStream);
+            Console.WriteLine("Hash produced. (through md5)");
+
+            var result = rsa.VerifyHash(hash, File.ReadAllBytes(signName),
+                HashAlgorithmName.MD5, RSASignaturePadding.Pkcs1);
+            Console.WriteLine("Signature checked.");
+
+            Console.WriteLine();
+
+            if (result)
+                Console.WriteLine("Certification passed.");
+            else
+                Console.WriteLine("Certification failed.");
+
+            Console.WriteLine();
+
+            Console.Write("Press enter to exit: ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SelfCertification/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SelfCertification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSA rsa = RSA.Create();
+
+            Console.Write("Enter the path of your private key: ");
+            var key = Console.ReadLine();
+            rsa.ImportRSAPrivateKey(File.ReadAllBytes(key), out _);
+
+            Console.Write("Enter the path of your file: ");
+            var fileName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            using FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            var hash = MD5.Create().ComputeHash(fileStream);
+            Console.WriteLine("Hash produced. (through md5)");
+
+            var sign = rsa.SignHash(hash, HashAlgorithmName.MD5, RSASignaturePadding.Pkcs1);
+            Console.WriteLine("Signature produced.");
+
+            File.WriteAllBytes("signature", sign);
+            Console.WriteLine("Signature saved at './signature'.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Here are the base64 value of the signature: ");
+            Console.WriteLine(Convert.ToBase64String(sign));
+
+            Console.WriteLine();
+
+            Console.Write("Press enter to exit: ");
+            Console.ReadLine();
+        }
+    }
+}
